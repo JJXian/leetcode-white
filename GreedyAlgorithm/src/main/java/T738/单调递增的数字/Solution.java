72f1a46d6d4a6d2e1fa319f9b29c6328a7c57a87
jingxian <2451999059@qq.com>
2024-06-17 16:16:00 +0800

    T738.单调递增的数字

---
+++ GreedyAlgorithm/src/main/java/T738/单调递增的数字/Solution.java
@@ -0,0 +1,34 @@
+package T738.单调递增的数字;
+
+import java.util.Scanner;
+
+/**
+ * @Author: jjxian
+ * @CreateTime: 2024-06-17
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入整数：");
+        int  n = sc.nextInt();
+        int res = monotoneIncreasingDigits(n);
+        System.out.println("结果是：" + res);
+
+    }
+
+    public static int monotoneIncreasingDigits(int n){
+        String[] s = (n+"").split("");
+
+        int start = s.length;
+        for(int i = s.length-1;i>0;i--){
+            if(Integer.parseInt(s[i]) <Integer.parseInt(s[i-1])){
+                s[i-1] = Integer.parseInt(s[i-1])-1+"";
+                start = i;
+            }
+        }
+        for(int i = start;i<s.length;i++){
+            s[i] = "9";
+        }
+        return Integer.parseInt(String.join("",s));
+    }
+}
